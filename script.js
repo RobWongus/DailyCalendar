b1bda06e9887f60b61a1b4010c9276354d6da434
Robert Wongus <rob_wo@yahoo.com>
2020-02-28 22:20:54 -0500

    redblock color

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-window.addEventListener('DOMContentLoaded', (event) => {
     let button = document.getElementById("eightam-button");  
-    let appointment= localStorage.getItem('');
-button.addEventListener("click", function(event){
-    var cat = localStorage.getItem('myCat');
-})
-
-
-// let d = new Date(2002, 5, 24, 18, 30);
-
-moment().format("dddd")
-
-// moment().format("DD/MM/YYYY")
-
-
-
+    let dateArray = document.getElementsByClassName("input-group-prepend");
+    let currentTime = moment().format("H");
+    let date = moment().format("dddd, DD/MM/YYYY");
+    $("#date").text(date);
+    
+    function appointmentColor(currentTime) {
+        for(let i = 0; i < dateArray.length; i++) {
+            if (currentTime - 8 > i){
+                dateArray[i].classList.add("redblock");
+            }
+        }  
+    }
 
 
 
-});
+    // let appointment= localStorage.getItem('');
 
+    // button.addEventListener("click", function(event){
+    
+    
+    
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var cat = localStorage.getItem('myCat');
