432573286c981e50e7b852d7e1d9b63f04713229
Robert Wongus <rob_wo@yahoo.com>
2020-02-28 21:13:23 -0500

    adding dates

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     let button = document.getElementById("eightam-button");  
-
+    let appointment= localStorage.getItem('');
 button.addEventListener("click", function(event){
-    console.log(event)
+    var cat = localStorage.getItem('myCat');
 })
 
 
+// let d = new Date(2002, 5, 24, 18, 30);
 
+moment().format("dddd")
 
-
+// moment().format("DD/MM/YYYY")
 
 
 
