026d3f477806db77ca9619f166daec2385c451ea
Robert Wongus <rob_wo@yahoo.com>
2020-02-28 20:08:49 -0500

    update HTML buttons

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    let button = document.getElementById("eightam-button");  
+
+button.addEventListener("click", function(event){
+    console.log(event)
+})
+
+
+
+
+
+
+
+
+
+
+
+});
 
