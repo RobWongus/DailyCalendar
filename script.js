ff12f8c08b52d7ce7195f8f18e4e79cdf65fac42
Robert Wongus <rob_wo@yahoo.com>
2020-02-28 23:41:26 -0500

    added local storage save functionality

--- script.js
+++ script.js
@@ -1,22 +1,49 @@
-    let button = document.getElementById("eightam-button");  
-    let dateArray = document.getElementsByClassName("input-group-prepend");
+let button = document.getElementById("eightam-button");  
+    let dateArray = document.getElementsByClassName("form-control");
     let currentTime = moment().format("H");
     let date = moment().format("dddd, DD/MM/YYYY");
+    let appointment = {};
+    if (window.localStorage.getItem(date) !== null) {
+        appointment = JSON.parse(window.localStorage.getItem(date));
+        for (let storage = 0; storage < dateArray.length; storage++) {
+            dateArray[storage].placeholder = appointment[storage];
+            
+        }
+
+    } 
     $("#date").text(date);
-    
+
     function appointmentColor(currentTime) {
-        for(let i = 0; i < dateArray.length; i++) {
+        for (let i = 0; i < dateArray.length; i++) {
             if (currentTime - 8 > i){
                 dateArray[i].classList.add("redblock");
+            } else if (currentTime - 8 === i) {
+                dateArray[i].classList.add("yellowblock");
+            } else {
+                dateArray[i].classList.add("greenblock");
             }
         }  
     }
 
+    appointmentColor(currentTime);
 
+    function save() {
+        for(let x = 0; x < dateArray.length; x++) {
+            appointment[x] = dateArray[x].value;
+            window.localStorage.setItem(date, JSON.stringify(appointment));
+        }
+    }
+        
+    document.getElementsByTagName("button").addEventListener("click", function(){
+        save();
+        console.log("save");
+    })          
+    
+    
+  
 
-    // let appointment= localStorage.getItem('');
-
-    // button.addEventListener("click", function(event){
+    
+    
     
     
     
@@ -35,4 +62,3 @@
 
 
 
-//var cat = localStorage.getItem('myCat');
